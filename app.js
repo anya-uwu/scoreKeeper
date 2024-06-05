9cbbe2a269aa95a212fa0739706a2293033bbb06
anya-uwu <anyadev@proton.me>
2024-06-05 14:50:44 -0400

    working on score keeper

---
+++ app.js
@@ -0,0 +1,10 @@
+const p1Button = document.querySelector('#p1Button');
+const p2Button = document.querySelector('#p2Button');
+
+p1Button.addEventListener('click', function () {
+
+});
+
+p2Button.addEventListener('click', function () {
+
+});
